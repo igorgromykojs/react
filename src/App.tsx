46666a0d54857cc9cf9bbaeb928e5c225dc302a9
igorgromykojs <125064212+igorgromykojs@users.noreply.github.com>
2023-10-31 08:04:48 +0300

    fix: use class Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, ReactElement } from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
 interface Planet {
@@ -7,89 +7,100 @@ interface Planet {
   population: string;
 }
 
-function App(): ReactElement {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState<Planet[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState<string>('');
-  const throwError = () => {
+interface AppProps {}
+
+interface AppState {
+  searchTerm: string;
+  searchResults: Planet[];
+  isLoading: boolean;
+  errorMessage: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      searchResults: [],
+      isLoading: true,
+      errorMessage: ''
+    };
+  }
+
+  throwError = () => {
     throw new Error('This is a sample error');
   };
 
-  const handleSearch = async (term: string) => {
-    setIsLoading(true);
+  handleSearch = async (term: string) => {
+    this.setState({ isLoading: true });
     try {
-      const response = await fetch(
-        `https://swapi.dev/api/planets/?search=${term}`
-      );
+      const response = await fetch(`https://swapi.dev/api/planets/?search=${term}`);
       const data = await response.json();
-      setSearchResults(data.results);
-      setIsLoading(false);
+      this.setState({ searchResults: data.results, isLoading: false, errorMessage: '' });
       localStorage.setItem('searchTerm', term);
-      setErrorMessage(``);
     } catch (error) {
-      setErrorMessage(`Error fetching data: ${error}`);
-      setIsLoading(false);
+      this.setState({ errorMessage: `Error fetching data: ${error}`, isLoading: false });
     }
   };
 
-  const fetchData = async () => {
-    setIsLoading(true);
+  fetchData = async () => {
+    this.setState({ isLoading: true });
     try {
       const response = await fetch(`https://swapi.dev/api/planets/`);
       const data = await response.json();
-      setSearchResults(data.results);
-      setIsLoading(false);
+      this.setState({ searchResults: data.results, isLoading: false });
     } catch (error) {
-      setErrorMessage(`Error fetching data: ${error}`);
-      setIsLoading(false);
+      this.setState({ errorMessage: `Error fetching data: ${error}`, isLoading: false });
     }
   };
 
-  useEffect(() => {
+  componentDidMount() {
     const savedSearchTerm = localStorage.getItem('searchTerm');
-
     if (savedSearchTerm) {
-      setSearchTerm(savedSearchTerm);
-      handleSearch(savedSearchTerm);
+      this.setState({ searchTerm: savedSearchTerm });
+      this.handleSearch(savedSearchTerm);
     } else {
-      fetchData();
+      this.fetchData();
     }
-  }, []);
+  }
 
-  return (
-    <div className="wrapper">
-      <div className="seach-panel">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button type="button" onClick={() => handleSearch(searchTerm)}>
-          Search
+  render() {
+    const { searchTerm, searchResults, isLoading, errorMessage } = this.state;
+
+    return (
+      <div className="wrapper">
+        <div className="search-panel">
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => this.setState({ searchTerm: e.target.value })}
+          />
+          <button type="button" onClick={() => this.handleSearch(searchTerm)}>
+            Search
+          </button>
+        </div>
+        <div>
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <ul>
+              {searchResults.map((planet) => (
+                <li key={planet.name}>
+                  <strong>Name:</strong> {planet.name} <br />
+                  <strong>Climate:</strong> {planet.climate} <br />
+                  <strong>Population:</strong> {planet.population}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+        <button type="button" onClick={this.throwError}>
+          Throw Error
         </button>
       </div>
-      <div>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          <ul>
-            {searchResults.map((planet) => (
-              <li key={planet.name}>
-                <strong>Name:</strong> {planet.name} <br />
-                <strong>Climate:</strong> {planet.climate} <br />
-                <strong>Population:</strong> {planet.population}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-      <button type="button" onClick={throwError}>
-        Throw Error
-      </button>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
