2a69ad0b9c3f353f3ee6ea3f96fb68e3311d69c5
igorgromykojs <125064212+igorgromykojs@users.noreply.github.com>
2023-11-02 22:30:26 +0300

    feat: husky correct

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -7,18 +7,27 @@ interface PaginationProps {
   totalPages: number;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, onPageChange, totalPages }) => {
-
+const Pagination: React.FC<PaginationProps> = ({
+  currentPage,
+  onPageChange,
+  totalPages,
+}) => {
   return (
     <div className="pagination">
       {currentPage > 1 && (
-        <Link to={`/?page=${currentPage - 1}`} onClick={() => onPageChange(currentPage - 1)}>
+        <Link
+          to={`/?page=${currentPage - 1}`}
+          onClick={() => onPageChange(currentPage - 1)}
+        >
           Previous
         </Link>
       )}
       <span>Current Page: {currentPage}</span>
       {currentPage < totalPages && (
-        <Link to={`/?page=${currentPage + 1}`} onClick={() => onPageChange(currentPage + 1)}>
+        <Link
+          to={`/?page=${currentPage + 1}`}
+          onClick={() => onPageChange(currentPage + 1)}
+        >
           Next
         </Link>
       )}
